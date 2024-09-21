8b8e5856bba30fdd4e8865267c2cc0f7b95b80b1
fayakuun27 <ayafaya27@gmail.com>
2024-09-21 14:15:24 +0700

    week 2 testimonial by using ajax

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -22,16 +22,26 @@ async function allTestimonial() {
   );
 
   if (!testimonials.length) {
-    return (document.getElementById("testimonials").innerHTML =
-      `<h1>Data not found!</h1>`);
+    return (document.getElementById(
+      "testimonials"
+    ).innerHTML = `<h1>Data not found!</h1>`);
   }
 
   const testimonialHTML = testimonials.map((testimonial) => {
-    return `<div class="testimonial">
-                  <img src="${testimonial.image}" class="profile-testimonial" />
-                  <p class="quote">"${testimonial.content}"</p>
-                  <p class="author">- ${testimonial.author}</p>
-              </div>`;
+    return `<div class="col">
+    <div class="card">
+      <img src="${testimonial.image}" class="card-img-top object-fit-cover" alt="..." />
+      <div class="card-body">
+        <p class="card-text fst-italic">
+          "${testimonial.content}"
+        </p>
+        <p class="text-end fw-bold">-${testimonial.author} Company</p>
+        <p class="text-end fw-bold">
+          ${testimonial.rating} <i class="fa-solid fa-star"></i>
+        </p>
+      </div>
+    </div>
+  </div>`;
   });
 
   document.getElementById("testimonials").innerHTML = testimonialHTML.join("");
@@ -47,16 +57,26 @@ async function filterTestimonial(rating) {
   );
 
   if (!filteredTestimonial.length) {
-    return (document.getElementById("testimonials").innerHTML =
-      `<h1>Data not found!</h1>`);
+    return (document.getElementById(
+      "testimonials"
+    ).innerHTML = `<h1>Data not found!</h1>`);
   }
 
   const testimonialHTML = filteredTestimonial.map((testimonial) => {
-    return `<div class="testimonial">
-                      <img src="${testimonial.image}" class="profile-testimonial" />
-                      <p class="quote">"${testimonial.content}"</p>
-                      <p class="author">- ${testimonial.author}</p>
-                  </div>`;
+    return `<div class="col">
+    <div class="card">
+      <img src="${testimonial.image}" class="card-img-top object-fit-cover" alt="..." />
+      <div class="card-body">
+        <p class="card-text fst-italic">
+          "${testimonial.content}"
+        </p>
+        <p class="text-end fw-bold">-${testimonial.author} Company</p>
+        <p class="text-end fw-bold">
+          ${testimonial.rating} <i class="fa-solid fa-star"></i>
+        </p>
+      </div>
+    </div>
+  </div>`;
   });
 
   document.getElementById("testimonials").innerHTML = testimonialHTML.join("");
