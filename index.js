bca82872206511eae9edcef9ac66fffef892a18c
fayakuun27 <ayafaya27@gmail.com>
2024-09-22 15:56:04 +0700

    week 3-4 copy dulu debugging kemudian

---
+++ index.js
@@ -0,0 +1,240 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const path = require("path");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+const { Sequelize, QueryTypes } = require("sequelize");
+const config = require("./config/config.json");
+const upload = require("./middlewares/upload-file");
+
+const sequelize = new Sequelize(config.development);
+
+const blogModel = require("./models").blog;
+const userModel = require("./models").user;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    name: "my-session",
+    secret: "ewVsqWOyeb",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(flash());
+
+app.get("/", home);
+app.get("/blog", blog);
+app.get("/add-project", addProjectView);
+app.post("/blog", upload.single("image"), addBlog);
+app.get("/delete-blog/:id", deleteBlog);
+app.get("/edit-blog/:id", editBlogView);
+app.post("/edit-blog/:id", editBlog);
+app.get("/contact", contact);
+app.get("/testimonials", testimonial);
+app.get("/blog-detail/:id", blogDetail);
+
+app.get("/login", loginView);
+app.get("/register", registerView);
+
+app.post("/register", register);
+app.post("/login", login);
+
+function loginView(req, res) {
+  res.render("login");
+}
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // cek email user apakah ada di database
+    const user = await userModel.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      req.flash("error", "Email / password salah!");
+      return res.redirect("/login");
+    }
+
+    // cek password apakah valid dengan password yang sudah di hash
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      req.flash("error", "Email / password salah!");
+      return res.redirect("/login");
+    }
+
+    req.session.user = user;
+
+    req.flash("success", "Login berhasil!");
+
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    res.redirect("/");
+  }
+}
+
+function registerView(req, res) {
+  res.render("register");
+}
+
+async function register(req, res) {
+  try {
+    const { name, email, password } = req.body;
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    await userModel.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    req.flash("success", "Register berhasil!");
+    res.redirect("/register");
+  } catch (error) {
+    req.flash("error", "Register gagal!");
+    res.redirect("/register");
+  }
+}
+
+function home(req, res) {
+  const user = req.session.user;
+
+  res.render("index", { user });
+}
+
+async function blog(req, res) {
+  // const query = `SELECT public.blogs.*, public.users.name AS username FROM public.blogs INNER JOIN public.users
+  // ON public.blogs."userId" = public.users.id;`;
+  // const result = await sequelize.query(query, { type: QueryTypes.SELECT });
+  const result = await blogModel.findAll({
+    include: [
+      {
+        model: userModel,
+      },
+    ],
+  });
+
+  const user = req.session.user;
+
+  res.render("blog", { data: result, user });
+}
+
+async function deleteBlog(req, res) {
+  const { id } = req.params;
+
+  let result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+
+  await blogModel.destroy({
+    where: {
+      id: id,
+    },
+  });
+  res.redirect("/blog");
+}
+
+async function addBlog(req, res) {
+  const { title, content } = req.body;
+  const imagePath = req.file.path;
+  const userId = req.session.user.id;
+
+  await blogModel.create({
+    title: title,
+    content: content,
+    image: imagePath,
+    userId: userId,
+  });
+
+  res.redirect("/blog");
+}
+
+async function editBlogView(req, res) {
+  const { id } = req.params;
+
+  const result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+
+  res.render("edit-blog", { data: result });
+}
+
+async function editBlog(req, res) {
+  const { id } = req.params;
+  const { title, content } = req.body;
+
+  const blog = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!blog) return res.render("not-found");
+
+  blog.title = title;
+  blog.content = content;
+
+  await blog.save();
+
+  res.redirect("/blog");
+}
+
+function addProjectView(req, res) {
+  const user = req.session.user;
+
+  if (!user) {
+    return res.redirect("/login");
+  }
+
+  res.render("add-project");
+}
+
+function contact(req, res) {
+  res.render("contact");
+}
+
+function testimonial(req, res) {
+  res.render("testimonials");
+}
+
+async function blogDetail(req, res) {
+  const { id } = req.params;
+  const result = await blogModel.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!result) return res.render("not-found");
+  res.render("blog-detail", { data: result });
+}
+
+app.listen(port, () => {
+  console.log("Server is running on PORT :", port);
+});
